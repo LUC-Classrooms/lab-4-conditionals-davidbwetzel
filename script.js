c89861e17d2057c913b561a9be171770d819e10d
davidbwetzel <david@wetzel-soyster.net>
2024-02-02 16:45:18 +0000

    Lab 4 Complete

--- script.js
+++ script.js
@@ -8,11 +8,12 @@ function draw() {
   // check to condition of the mouse
   if(mouseIsPressed){ // true or false?
     // if true, change the fill color here
-    fill("blue");
+    fill(255, 0, 0);
+
   }
   else { 
     // otherwise do this:
-    fill("yellow");
+    fill(0, 255, 0);
   }
   ellipse(width/2, height/2, 100, 100);
 }
